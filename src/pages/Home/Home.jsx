b225160d74f7017f9687ed3223adbcef9ee77f53
pepu <pepshove@gmail.com>
2025-07-25 14:51:13 +0200

    by pepu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,17 @@
-
 import Header from "../../components/Header/Header";
 import "./Home.css";
 import ThabangArtz from "../About/ThabangArtz";
 import Footer from "../../components/Footer/Footer";
-
-
 
 function Home(){
     return(
         <>
         <Header />
         <div className="home-intro">
-            <h1>"Crafting Timeless Stories in Stone</h1>
-            <h3>Where Vision and Passion Meet."</h3>
+            <h1>&quot;Crafting Timeless Stories in Stone&quot;</h1>
+            <h3>Where Vision and Passion Meet.</h3>
             <div className="glry-cln">
-                 <a href="/gallery" className="btn">Explore My Gallery</a>
+                <a href="/gallery" className="btn">Explore My Gallery</a>
             </div>
         </div>
         <ThabangArtz />
@@ -22,4 +19,5 @@ function Home(){
         </>
     );
 }
+
 export default Home;
